fff5d670a614a7a538c1e1d7dfa0f4a77b4cce67
huangrui <1194459011@qq.com>
2025-12-16 23:51:20 +0800

    引入spdlog，对其进行简单封装以便作为项目的日志模块使用

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -7,11 +7,13 @@ extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv)
 {
-	printf("Hazel Engine Start!\n");
+	Hazel::Log::Init();
+	HZ_CORE_WARN("Initialized Log!");
+	int a = 10;
+	HZ_INFO("Welcome to Hazel!");
 	auto app = Hazel::CreateApplication();
 	app->Run();
 	delete app;
 }
-
 
 #endif // HZ_PLATFORM_WINDOWS
